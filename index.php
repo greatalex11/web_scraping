5040cb92f8ff31e595ce886e7ba468491c228fae
Alex <alexguerillot1@gmail.com>
2024-12-23 19:11:04 +0100

    partie front ok:fetch géré

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-/**
- *
- *  bot de web scraping en php - bibliotheque Guzzle
+/*
+ *  bot de web scraping en php - bibliotheque Guzzle 
  *
 */
 
@@ -10,17 +9,45 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 require('index.html');
+
+use GuzzleHttp\Client;  // Utilisation d'une dépendance installée via Composer
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+
+
 session_start();
-if($_SESSION){
 
+/* Récupération de la Request dans le flux entrant */
+
+        //phpinfo(); visualisation état de la config, curl enable?
+
+
+
+$data = file_get_contents('php://input'); 
+var_dump($data);
+$uri= json_decode($data, true);
+
+echo $uri;
+if ($uri) {
+    echo "data serveur:" + " " + $uri;
+} else {
+    echo "Aucune URI reçue.";
 }
-$uri='https://exemple.fr';
 
-// Utilisation d'une dépendance installée via Composer
-use GuzzleHttp\Client;
+/* Gestion flux sortant avec CURL
+   curl_setopt($ch, CURLOPT_POST,true);
+  $http=curl_getinfo($ch,CURLINFO_HTTP_CODE);
+echo $data;
 
-$client = new Client();
-$response = $client->get($uri);
-echo $response->getBody();
+$uri = $_POST['uri'] ?? null;
+
+if ($uri) {
+    echo "URI reçue via \$_POST : " . $uri;
+} else {
+    echo "zero.";
+}
+*/
+
+// Gestion flux sortant avec GUZZLE //
 
 ?>
